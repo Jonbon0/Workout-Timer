eed17bf4b704a361ff2c4ac4587e68e1e333f160
Jonah <jonahc99@gmail.com>
2025-09-24 13:13:50 -0700

    Initial Commit

---
+++ Workout Timer/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Workout Timer
+//
+//  Created by Jonah Cheung on 2025-09-24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Workout Timer/Workout_TimerApp.swift
@@ -0,0 +1,17 @@
+//
+//  Workout_TimerApp.swift
+//  Workout Timer
+//
+//  Created by Jonah Cheung on 2025-09-24.
+//
+
+import SwiftUI
+
+@main
+struct Workout_TimerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
